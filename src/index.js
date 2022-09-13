bdd4e2df9c0bb51b6e3cc9169d99e38e03f84597
cliffclimber-721 <cliffclimber@g.skku.edu>
2022-09-13 17:03:33 +0900

    only metamask now

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import MetamaskWallet from "./MetamaskWallet";
-import KaikasWallet from "./KaikasWallet";
+//import KaikasWallet from "./KaikasWallet";
 
 ReactDOM.render(
   <React.StrictMode>
     <MetamaskWallet />
-    <KaikasWallet />
   </React.StrictMode>,
   document.getElementById("root")
 );
